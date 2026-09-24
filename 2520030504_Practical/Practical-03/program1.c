6428eb528b57e9c9aeba0d45d2c7780d7f8f19e8
shreenithadevuni <shreenithadevuni2203@gmail.com>
2026-09-24 08:13:36 +0530

    Implement fork to create child and parent processes

--- 2520030504_Practical/Practical-03/program1.c
+++ 2520030504_Practical/Practical-03/program1.c
@@ -1,1 +1,19 @@
-
+#include<unistd.h>
+#include<stdio.h>
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0){
+printf("Child Process\n");
+printf("Child pid is %d\n",getpid());
+}
+else if(pid>0){
+printf("Parent Process\n");
+printf("Parent pid is %d\n",getpid());
+}
+else if(pid<0)
+{
+printf("Failed process\n");
+}
+}
